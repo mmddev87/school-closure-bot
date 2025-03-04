481427dba1f0b12d4aab5aea0b7204af26d36c77
Mohammad Taha <saghafi.mt@gmail.com>
2025-03-04 18:13:32 +0330

    Summarize imports in handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -1,9 +1,9 @@
 from telegram.ext import Application, CommandHandler
-import src.commands.start
-import src.commands.set_reminder
-import src.commands.check
+from .commands import start
+from .commands import set_reminder
+from .commands import check
 
 def register_handlers(app: Application)-> None:
-    app.add_handler(CommandHandler("start", src.commands.start.handle))
-    app.add_handler(CommandHandler("setreminder", src.commands.set_reminder.handle))
-    app.add_handler(CommandHandler("check", src.commands.check.handle))
+    app.add_handler(CommandHandler("start", start.handle))
+    app.add_handler(CommandHandler("setreminder", set_reminder.handle))
+    app.add_handler(CommandHandler("check", check.handle))
