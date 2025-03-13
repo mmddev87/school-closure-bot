1c66bfb17893f75c3f33fd732e2415cd34312ffd
Mohammad Taha <saghafi.mt@gmail.com>
2025-03-13 16:41:29 +0330

    Remove python codes

--- src/commands/set_reminder.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def handle(update: Update, callback: ContextTypes.DEFAULT_TYPE):
-    print('set_reminder')
--- src/handlers.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import Application, CommandHandler
-from .commands import start
-from .commands import set_reminder
-from .commands import check
-
-def register_handlers(app: Application)-> None:
-    app.add_handler(CommandHandler("start", start.handle))
-    app.add_handler(CommandHandler("setreminder", set_reminder.handle))
-    app.add_handler(CommandHandler("check", check.handle))
